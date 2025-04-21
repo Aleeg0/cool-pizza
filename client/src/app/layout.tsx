b0d72550d24667a805d356481ae031679527aea6
Aleeg0 <Sankafoxe@gmail.com>
2025-04-21 23:59:41 +0300

    feat(client/src/app): add layout component Add entry point for client application, add metadata for SEO and google fonts

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Nunito} from "next/font/google";
+import type { Metadata } from "next";
+import '@/styles/index.scss';
+
+const nunitoFont = Nunito({subsets: ["cyrillic"]})
+
+export const metadata: Metadata = {
+  title: "Cool pizza",
+  description: "Pet-project \"cool pizza\" grate example of ASP.NET Core Web and NextJs web-application ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={nunitoFont.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
