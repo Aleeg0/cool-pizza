e5b2201e575ab4b5db46f15bc9cd930dedf781b4
Aleeg0 <Sankafoxe@gmail.com>
2025-04-22 18:06:53 +0300

    feat(client/src/components/ui/Button): add UiButton component Add flexible button component for UI with styles

---
+++ client/src/components/ui/Buttons/UiButton.tsx
@@ -0,0 +1,48 @@
+import React, {FC} from 'react';
+import {cn} from "@/utils";
+
+
+interface ButtonProps {
+  caption?: string;
+  icon?: React.ReactNode;
+  iconSize?: "s" | "m" | "l";
+  type?: "primary" | "secondary";
+  size?: "s" | "m" | "l"
+  onClick?: () => void;
+}
+
+const UiButton : FC<ButtonProps> = ({
+  caption,
+  icon,
+  iconSize = "m",
+  type = "primary",
+  size = "m",
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "UiButton_root",
+        `UiButton_${type}`,
+        `UiButton-${size}`,
+      )}
+    >
+      {icon &&
+        <span className={cn(
+          "UiButton_icon",
+          `UiKit_icon-${iconSize}`,
+        )}>
+          {icon}
+        </span>
+      }
+      {caption &&
+        <span className={"UiButton_caption"}>
+          {caption}
+        </span>
+      }
+    </button>
+  );
+};
+
+export default UiButton;
