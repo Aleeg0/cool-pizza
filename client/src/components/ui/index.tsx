41088b25562331da1d7a112facab69586183e77f
Aleego <Sankafoxe@gmail.com>
2025-04-23 23:56:29 +0300

    feat(client/src/components/ui): add new ui components to re-export

--- client/src/components/ui/index.tsx
+++ client/src/components/ui/index.tsx
@@ -1,2 +1,6 @@
 export {HeaderTitle} from './HeaderTitle/HeaderTitle';
 export {Input} from './Input/Input';
+export {UiButton} from "./Buttons/UiButton"
+export {InlineButton} from './Buttons/InlineButton'
+export {UiCheckbox} from './Checkbox/UiCheckbox'
+export {AppPopup} from './AppPopup/AppPopup'
