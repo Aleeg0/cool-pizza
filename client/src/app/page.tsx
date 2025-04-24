ba3fb6f5144b1dc683303b28f7454202e97c3836
Aleego <Sankafoxe@gmail.com>
2025-04-24 11:27:46 +0300

    feat(client/src/app): add some components to page and add styles some for page Add Header, Categories, Sorter to home page, and test main block without styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,38 @@
-import Header from "@/components/layout/Header/Header";
+import {Header} from "@/components/layout";
+import {Categories, Sorter} from "@/components/features";
+import styles from './Home.module.scss'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>
-        <Header/>
+    <div className={styles.HomePage_root}>
+      <Header/>
+      <div className={styles.HomePage_container}>
+        <div className={styles.HomePage_title}>
+          <h1>Все пиццы</h1>
+        </div>
+        <div className={styles.HomePage_filtration}>
+          <Categories/>
+          <Sorter/>
+        </div>
+        <main className={styles.HomePage_content}>
+
+        </main>
+        {/*<div id="meat" style={{height: "400px", background: "orange"}}>
+
+        </div>
+        <div id="spicy" style={{height: "400px", background: "green"}}>
+
+        </div>
+        <div id="sweet" style={{height: "400px", background: "blue"}}>
+
+        </div>
+        <div id="vegetarian" style={{height: "400px", background: "red"}}>
+
+        </div>
+        <div id="with_chicken" style={{height: "400px", background: "black"}}>
+
+        </div>*/}
+      </div>
     </div>
   );
 }
