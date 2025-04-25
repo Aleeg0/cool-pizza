3c3194b61d8490edd17e8a6d502a28f8758c05f4
Aleego <Sankafoxe@gmail.com>
2025-04-25 11:16:25 +0300

    fix(client/src/app): update structure of home page and implements ProductCatalog

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,43 +1,30 @@
 import {Header} from "@/components/layout";
-import {Categories, Sorter, Filtration} from "@/components/features";
+import {Categories, Sorter, Filtration, ProductCatalog} from "@/components/features";
 import styles from './Home.module.scss'
 
 export default function HomePage() {
   return (
     <div className={styles.HomePage_root}>
       <Header/>
-      <div className={styles.HomePage_container}>
-        <div className={styles.HomePage_title}>
-          <h1>Все пиццы</h1>
-        </div>
-        <div className={styles.HomePage_categoriesSort}>
-          <Categories/>
-          <Sorter/>
-        </div>
-        <main className={styles.HomePage_content}>
-          <div className={styles.HomePage_filtration}>
-            <Filtration/>
+      <main className={styles.HomePage_container}>
+        <div className={styles.HomePage_contentWrapper}>
+          <div className={styles.HomePage_title}>
+            <h1>Все пиццы</h1>
           </div>
-          <div className={styles.HomePage_productList}>
-
+          <div className={styles.HomePage_categoriesSort}>
+            <Categories/>
+            <Sorter/>
           </div>
-        </main>
-        {/*<div id="meat" style={{height: "400px", background: "orange"}}>
-
+          <div className={styles.HomePage_content}>
+            <div className={styles.HomePage_filtration}>
+              <Filtration/>
+            </div>
+            <main className={styles.HomePage_productCatalog}>
+              <ProductCatalog/>
+            </main>
+          </div>
         </div>
-        <div id="spicy" style={{height: "400px", background: "green"}}>
-
-        </div>
-        <div id="sweet" style={{height: "400px", background: "blue"}}>
-
-        </div>
-        <div id="vegetarian" style={{height: "400px", background: "red"}}>
-
-        </div>
-        <div id="with_chicken" style={{height: "400px", background: "black"}}>
-
-        </div>*/}
-      </div>
+      </main>
     </div>
   );
 }
