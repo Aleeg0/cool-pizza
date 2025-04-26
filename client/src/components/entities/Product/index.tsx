d02fba57c332436ab81779c3a5e97b0bf90b4a32
Aleego <Sankafoxe@gmail.com>
2025-04-27 00:03:24 +0300

    feat(client/src/components/entities/Product): add export index for all components in product folder

---
+++ client/src/components/entities/Product/index.tsx
@@ -0,0 +1,4 @@
+export {default as ProductCard} from './Card/Card';
+export {default as ProductModal} from './Modal/Modal';
+export {default as ProductCatalog} from './Catalog/Catalog';
+export {default as SimpleProductModule} from './SimpleModal/SimpleModal';
