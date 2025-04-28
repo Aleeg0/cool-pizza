106bad098a0466bdd77a7ec0abfa946d5626fa44
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:00:39 +0300

    feat(server/Core/Entities): add properties for PizzaProduct entity

---
+++ server/CoolPizza.Core/Entities/PizzaProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace CoolPizza.Core.Entities;
+
+public class PizzaProduct: Entity
+{
+    public string ImgUrl { get; private set; }
+    public decimal Price { get; private set; }
+    public int Size {get; private set;}
+    public string Dough { get; private set; }
+    public double? Weight { get; private set; }
+    public Product Product { get; private set; }
+}
