4daaabc7d91ea39308b94ceef4bb589b1f2c227f
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:00:46 +0300

    feat(server/Core/Entities): add properties for RegularProduct entity

---
+++ server/CoolPizza.Core/Entities/RegularProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolPizza.Core.Entities;
+
+public class RegularProduct: Entity
+{
+    public string ImgUrl { get; private set; }
+    public decimal Price { get; private set; }
+    public string Details { get; private set; }
+    public double? Weight { get; private set; }
+    public Product Product { get; private set; }
+}
