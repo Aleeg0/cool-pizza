476837361db17cfadeb03c15f90d6c039aa07ab8
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:00:31 +0300

    feat(server/Core/Entities): add properties for Product entity

---
+++ server/CoolPizza.Core/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using CoolPizza.Core.Enums;
+
+namespace CoolPizza.Core.Entities;
+
+public class Product: Entity
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string baseImg { get; private set; }
+    public string basePrice { get; private set; }
+    public ProductType Type { get; private set; }
+    public Category Category { get; private set;}
+    public ICollection<RegularProduct> RegularProducts { get; private set; }
+    public ICollection<PizzaProduct> PizzaProducts { get; private set; }
+}
