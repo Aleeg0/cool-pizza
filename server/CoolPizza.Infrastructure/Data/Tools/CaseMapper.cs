a0e966fe0e8efaea428fbf28c4dbf46c344233cf
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:15:50 +0300

    feat(server/Infrastructure): add CaseMapper static class with ToSnakeCase method to map entity properties to snake case in db

---
+++ server/CoolPizza.Infrastructure/Data/Tools/CaseMapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace CoolPizza.Infrastructure.Data.Tools;
+
+public static class CaseMapper
+{
+    public static string ToSnakeCase(string? input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return "";
+
+        var result = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char c = input[i];
+            if (char.IsUpper(c))
+            {
+                if (i > 0)
+                    result.Append('_');
+                result.Append(char.ToLower(c));
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+        return result.ToString();
+    }
+}
