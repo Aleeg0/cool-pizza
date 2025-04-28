f5164def058b9c62b887d4ff2e187f19e1fe1fca
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:00:16 +0300

    feat(server/Core/Entities): add properties for Category entity

---
+++ server/CoolPizza.Core/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CoolPizza.Core.Entities;
+
+public class Category : Entity
+{
+    public string Name { get; private set; }
+    public string Value { get; private set; }
+    public ICollection<Product> Products { get; private set; }    
+}
