1b4c8423e67941a6570ef510679be242294e9a5d
Aleego <Sankafoxe@gmail.com>
2025-04-28 13:00:25 +0300

    feat(server/Core/Entities): add properties for Ingredient entity

---
+++ server/CoolPizza.Core/Entities/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace CoolPizza.Core.Entities;
+
+public class Ingredient: Entity
+{
+    public string Name {get; private set;}
+    public string ImgUrl {get; private set;}
+    public string Price {get; private set;}
+}
