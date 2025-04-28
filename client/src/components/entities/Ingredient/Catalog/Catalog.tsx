90d92e895f14bc5c4b0e242f1a1cf38dec554271
Aleego <Sankafoxe@gmail.com>
2025-04-28 11:19:33 +0300

    feat(client/entities/Ingredient/Catalog): implement redux logic

--- client/src/components/entities/Ingredient/Catalog/Catalog.tsx
+++ client/src/components/entities/Ingredient/Catalog/Catalog.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {IngredientCard} from "@/components/entities/Ingredient";
 import styles from './styles.module.scss'
+import {Ingredient} from "@/store/types/Ingredient";
 
-const Catalog = () => {
+interface Props {
+  ingredients: Ingredient[];
+  selectedIds: string[];
+  onSelect: (id: string) => void;
+}
+
+const Catalog: FC<Props> = ({ingredients, selectedIds, onSelect}) => {
 
 
   return (
@@ -11,15 +18,13 @@ const Catalog = () => {
         <h4>Добавить по вкусу</h4>
       </div>
       <div className={styles.ingredients}>
-        {Array.from(Array(10).keys()).map((_, i) =>
+        {ingredients.map((ingredient) =>
         <div
-          key={i}
+          key={ingredient.id}
           className={styles.ingredient}
         >
           <IngredientCard
-            name="Сырный бортик"
-            imgUrl="/images/testIngredient.png"
-            price={179}
+            {...ingredient}
             currency="₽"
           />
         </div>
