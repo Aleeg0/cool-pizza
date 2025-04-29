e3fe63e1fc1791afcd75f1458663163db95eabe6
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:48:53 +0300

    ref(server/Core/Entities): update Product entity Move to folder Products, change property Category to Category id to optimize db, change name of foreign entities

---
+++ server/CoolPizza.Core/Entities/Products/Product.cs
@@ -0,0 +1,17 @@
+﻿using CoolPizza.Core.Enums;
+
+namespace CoolPizza.Core.Entities.Products;
+
+public class Product: Entity
+{
+    public const int NameMaxLength = 128;
+    
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string BaseImg { get; private set; }
+    public string BasePrice { get; private set; }
+    public ProductType Type { get; private set; }
+    public Guid CategoryId { get; private set;}
+    public ICollection<Goods> Goods { get; private set; }
+    public ICollection<Pizza> Pizzas { get; private set; }
+}
