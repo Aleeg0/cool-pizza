e4d0cd5d7d8004c423622e09d806e2d2d779c5ac
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:50:43 +0300

    feat(server/Core/Entities): add constants to Ingredients entity for configuration in DB, and Foreign entities

--- server/CoolPizza.Core/Entities/Ingredient.cs
+++ server/CoolPizza.Core/Entities/Ingredient.cs
@@ -1,8 +1,16 @@
-﻿namespace CoolPizza.Core.Entities;
+﻿using CoolPizza.Core.Entities.Orders;
+using CoolPizza.Core.Entities.Products;
+
+namespace CoolPizza.Core.Entities;
 
 public class Ingredient: Entity
 {
+    public const int MaxNameLength = 128;
+    
     public string Name {get; private set;}
     public string ImgUrl {get; private set;}
     public string Price {get; private set;}
+    
+    public ICollection<Pizza> Pizzas {get; private set;}
+    public ICollection<OrderedPizza> OrderedPizzas {get; private set;}
 }
