2f75843664e0423a4def14b7a236ac125a140e02
Aleego <Sankafoxe@gmail.com>
2025-04-29 14:57:44 +0300

    feat(server/Core/Entities): add constants for properties, and remove nullable types

--- server/CoolPizza.Core/Entities/User.cs
+++ server/CoolPizza.Core/Entities/User.cs
@@ -2,11 +2,16 @@
 
 namespace CoolPizza.Core.Entities;
 
-public class User
+public class User: Entity
 {
-    public string? Name {get; private set;} 
+    public const int NameMaxLength = 128;
+    public const int EmailMaxLength = 128;
+    public const int PasswordMaxLength = 32;
+    public const int PhoneMaxLength = 16;
+    
+    public string Name {get; private set;} 
     public string Email {get; private set;}
-    public string? Password {get; private set;}
+    public string Password {get; private set;}
     public string Phone {get; private set;} 
     
     public ICollection<Order> Orders {get; private set;}
