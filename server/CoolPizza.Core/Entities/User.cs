4e9294b3e2204b3542717f1a706efc0c623555e5
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:52:16 +0300

    feat(server/Core/Entities): add User entity

---
+++ server/CoolPizza.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using CoolPizza.Core.Entities.Orders;
+
+namespace CoolPizza.Core.Entities;
+
+public class User
+{
+    public string? Name {get; private set;} 
+    public string Email {get; private set;}
+    public string? Password {get; private set;}
+    public string Phone {get; private set;} 
+    
+    public ICollection<Order> Orders {get; private set;}
+}
