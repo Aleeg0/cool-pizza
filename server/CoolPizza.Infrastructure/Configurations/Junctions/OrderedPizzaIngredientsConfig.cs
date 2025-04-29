029357acaaacd88bcf449c57cdfed12e6459a67e
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:55:06 +0300

    feat(server/Infrastructure/Configurations/Orders): add DB config for OrderedPizzaIngredients and PizzaIngredients

---
+++ server/CoolPizza.Infrastructure/Configurations/Junctions/OrderedPizzaIngredientsConfig.cs
@@ -0,0 +1,22 @@
+﻿using CoolPizza.Core.Entities.Junctions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoolPizza.Infrastructure.Configurations.Junctions;
+
+public class OrderedPizzaIngredientsConfig: IEntityTypeConfiguration<OrderedPizzaIngredients>
+{
+    public void Configure(EntityTypeBuilder<OrderedPizzaIngredients> builder)
+    {
+        builder
+            .HasKey(opi => new { PizzaId = opi.OrderedPizzaId, opi.IngredientId });
+        
+        builder
+            .Property(opi => opi.OrderedPizzaId)
+            .HasColumnName("ordered_pizza_id");
+        
+        builder
+            .Property(opi => opi.IngredientId)
+            .HasColumnName("ingredient_id");
+    }
+}
