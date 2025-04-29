4c6e95779bc3a63e070b0655ca410ca835a852c7
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:46:32 +0300

    ref(server/Core/Entities): update RegularProduct entity Rename to Goods, move to folder Products, change property Product to ProductId to optimize db

--- server/CoolPizza.Core/Entities/Products/Goods.cs
+++ server/CoolPizza.Core/Entities/Products/Goods.cs
@@ -1,10 +1,10 @@
-﻿namespace CoolPizza.Core.Entities;
+﻿namespace CoolPizza.Core.Entities.Products;
 
-public class RegularProduct: Entity
+public class Goods: Entity
 {
     public string ImgUrl { get; private set; }
     public decimal Price { get; private set; }
     public string Details { get; private set; }
     public double? Weight { get; private set; }
-    public Product Product { get; private set; }
+    public Guid ProductId { get; private set; }
 }
