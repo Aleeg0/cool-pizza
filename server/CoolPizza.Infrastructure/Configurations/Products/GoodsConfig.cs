dc5bcd0263eb7a425d747b15bb5c0a3f4caed8cb
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:55:28 +0300

    feat(server/Infrastructure/Configurations/Products): add DB config for Product, Goods, Pizza

---
+++ server/CoolPizza.Infrastructure/Configurations/Products/GoodsConfig.cs
@@ -0,0 +1,48 @@
+﻿using CoolPizza.Core.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoolPizza.Infrastructure.Configurations.Products;
+
+public class GoodsConfig: IEntityTypeConfiguration<Goods>
+{
+    public void Configure(EntityTypeBuilder<Goods> builder)
+    {
+        builder.ToTable("goods");
+        
+        builder.HasKey(g => g.Id);
+        
+        builder
+            .Property(g => g.Id)
+            .HasColumnName("id")
+            .HasDefaultValueSql("gen_random_uuid()")
+            .ValueGeneratedOnAdd();
+        
+        builder
+            .Property(g => g.ImgUrl)
+            .HasColumnName("img_url");
+
+        builder
+            .Property(g => g.Price)
+            .HasColumnName("price")
+            .HasColumnType("decimal(5,2)");
+        
+        builder
+            .Property(g => g.Details)
+            .HasColumnName("details");
+        
+        builder
+            .Property(g => g.Weight)
+            .HasColumnName("weight");
+        
+        builder
+            .Property(g => g.ProductId)
+            .HasColumnName("product_id");
+        
+        builder
+            .HasOne<Product>()
+            .WithMany(g => g.Goods)
+            .HasForeignKey(g => g.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ server/CoolPizza.Infrastructure/Configurations/Products/PizzaConfig.cs
@@ -0,0 +1,53 @@
+﻿using CoolPizza.Core.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoolPizza.Infrastructure.Configurations.Products;
+
+public class PizzaConfig: IEntityTypeConfiguration<Pizza>
+{
+    public void Configure(EntityTypeBuilder<Pizza> builder)
+    {
+        builder.ToTable("pizzas");
+        
+        builder.HasKey(p => p.Id);
+        builder
+            .Property(p => p.Id)
+            .HasColumnName("id")
+            .HasDefaultValueSql("gen_random_uuid()")
+            .ValueGeneratedOnAdd();
+        
+        builder
+            .Property(p => p.ImgUrl)
+            .HasColumnName("img_url");
+
+        builder
+            .Property(p => p.Price)
+            .HasColumnName("price")
+            .HasColumnType("decimal(5,2)");;
+        
+        builder
+            .Property(p => p.Size)
+            .HasColumnName("size");
+        
+        builder
+            .Property(p => p.Dough)
+            .HasColumnName("dough")
+            .HasMaxLength(Pizza.DoughMaxLength);
+        
+        builder
+            .Property(p => p.Weight)
+            .HasColumnName("weight");
+        
+        builder
+            .Property(p => p.ProductId)
+            .HasColumnName("product_id");
+        
+        // relation with product 1:N
+        builder
+            .HasOne<Product>()
+            .WithMany(p => p.Pizzas)
+            .HasForeignKey(p => p.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
