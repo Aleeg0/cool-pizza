58336b460121fc02c0af7fce932353d70519ffa2
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 12:03:38 +0300

    feat(server/Infrastructure/Data): remove logic with CaseMapper and implement all configurations from current project

--- server/CoolPizza.Infrastructure/Data/ApplicationDbContext.cs
+++ server/CoolPizza.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using CoolPizza.Core.Entities;
-using CoolPizza.Infrastructure.Data.Tools;
+using CoolPizza.Core.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoolPizza.Infrastructure.Data;
@@ -15,41 +15,6 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
-        // изменяем code style для сущностей на SneakCase
-        ToSneakCaseAllEntities(modelBuilder);
-    }
-
-    private void ToSneakCaseAllEntities(ModelBuilder modelBuilder)
-    {
-        foreach (var entity in modelBuilder.Model.GetEntityTypes())
-        {
-            // Переименование таблиц
-            entity.SetTableName(CaseMapper.ToSnakeCase(entity.GetTableName()));
-
-            // Переименование всех колонок
-            foreach (var property in entity.GetProperties())
-            {
-                property.SetColumnName(CaseMapper.ToSnakeCase(property.Name));
-            }
-
-            // Переименование ключей
-            foreach (var key in entity.GetKeys())
-            {
-                key.SetName(CaseMapper.ToSnakeCase(key.GetName()));
-            }
-
-            // Переименование индексов
-            foreach (var index in entity.GetIndexes())
-            {
-                index.SetDatabaseName(CaseMapper.ToSnakeCase(index.GetDatabaseName()));
-            }
-
-            // Переименование внешних ключей
-            foreach (var foreignKey in entity.GetForeignKeys())
-            {
-                foreignKey.SetConstraintName(CaseMapper.ToSnakeCase(foreignKey.GetConstraintName()));
-            }
-        }
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
