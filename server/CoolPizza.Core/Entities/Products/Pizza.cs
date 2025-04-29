20698f73bcc497af07c02eb4d6c59b2d9e05734b
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:47:04 +0300

    ref(server/Core/Entities): update PizzaProduct entity Rename to Pizza, move to folder Products, change property Product to ProductId to optimize db, add Ingredients collection

---
+++ server/CoolPizza.Core/Entities/Products/Pizza.cs
@@ -0,0 +1,14 @@
+﻿namespace CoolPizza.Core.Entities.Products;
+
+public class Pizza: Entity
+{
+    public const int DoughMaxLength = 64;
+    
+    public string ImgUrl { get; private set; }
+    public decimal Price { get; private set; }
+    public int Size {get; private set;}
+    public string Dough { get; private set; }
+    public double? Weight { get; private set; }
+    public Guid ProductId { get; private set; }
+    public ICollection<Ingredient> Ingredients { get; private set; }
+}
