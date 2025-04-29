19233f4a502bcb455e8a3c283b43133567a97380
Aleeg0 <Sankafoxe@gmail.com>
2025-04-29 11:49:47 +0300

    feat(server/Core/Entities): add constants to Category entity for configuration in DB

--- server/CoolPizza.Core/Entities/Category.cs
+++ server/CoolPizza.Core/Entities/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace CoolPizza.Core.Entities;
+﻿using CoolPizza.Core.Entities.Products;
+
+namespace CoolPizza.Core.Entities;
 
 public class Category : Entity
 {
+    public const int NameMaxLength = 64;
+    public const int ValueMaxLength = 64;
+    
     public string Name { get; private set; }
     public string Value { get; private set; }
     public ICollection<Product> Products { get; private set; }    
