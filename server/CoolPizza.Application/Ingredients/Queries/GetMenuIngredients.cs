a0f47392151d31da0d5d44cd7fbd8986296d2877
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:02:02 +0300

    feat(server/Application/Ingredients): add query GetMenuIngredients to get all ingredients for menu

---
+++ server/CoolPizza.Application/Ingredients/Queries/GetMenuIngredients.cs
@@ -0,0 +1,27 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.DTOs.Projections;
+using MediatR;
+
+namespace CoolPizza.Application.Ingredients.Queries;
+
+public record GetMenuIngredientsQuery : IRequest<List<MenuIngredientDto>> { }
+
+public class GetMenuIngredientsHandler(IIngredientsRepository ingredientsRepository) :
+    IRequestHandler<GetMenuIngredientsQuery, List<MenuIngredientDto>>
+{
+    public async Task<List<MenuIngredientDto>> Handle(GetMenuIngredientsQuery request, CancellationToken cancellationToken)
+    {
+        var ingredients = await ingredientsRepository.GetAllAsync();
+        
+        var result = ingredients
+            .Select(ingredient => new MenuIngredientDto
+                (
+                     ingredient.Id,
+                    ingredient.Name
+                )
+            )
+            .ToList();
+        
+        return result;
+    }
+}
