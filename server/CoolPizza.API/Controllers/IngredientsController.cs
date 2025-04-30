1be00fcdfe3969067ada70fc3a7e657644154535
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:13:50 +0300

    feat(server/API/Controllers): add IngredientsController with 1 Get request

---
+++ server/CoolPizza.API/Controllers/IngredientsController.cs
@@ -0,0 +1,19 @@
+﻿using CoolPizza.Application.Ingredients.Queries;
+using CoolPizza.Core.DTOs.Projections;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class IngredientsController(IMediator mediator) : ControllerBase
+{
+    
+    [HttpGet("menu")]
+    public async Task<ActionResult<List<MenuIngredientDto>>> GetMenuIngredients()
+    {
+        var ingredients = await mediator.Send(new GetMenuIngredientsQuery());
+        return ingredients;
+    }
+}
