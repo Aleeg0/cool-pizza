9f21bedb7537d6502a6ff80a82a1500b80fb7b51
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:01:02 +0300

    feat(server/Application/Exceptions): add custom NotFoundException

---
+++ server/CoolPizza.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolPizza.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+        : base("Entity not found.") { }
+
+    public NotFoundException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.") { }
+}
