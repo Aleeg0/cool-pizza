ca510b8916f6b01cfb7c2435820ac21e61d1fbc5
Aleego <Sankafoxe@gmail.com>
2025-04-30 09:54:51 +0300

    feat(server/Core/Abstractions): add interfaces for repositories

---
+++ server/CoolPizza.Core/Abstractions/IIngredientsRepository.cs
@@ -0,0 +1,8 @@
+﻿using CoolPizza.Core.DTOs.Projections;
+
+namespace CoolPizza.Core.Abstractions;
+
+public interface IIngredientsRepository
+{
+    Task<List<MenuIngredientDto>> GetAllAsync();
+}
---
+++ server/CoolPizza.Core/Abstractions/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoolPizza.Core.DTOs;
+using CoolPizza.Core.DTOs.Projections;
+using CoolPizza.Core.Entities.Products;
+using CoolPizza.Core.Enums;
+
+namespace CoolPizza.Core.Abstractions;
+
+public interface IProductsRepository
+{
+    Task<List<MenuProductDto>> GetMenuAllAsync(SortOption sortOption);
+    Task<List<MenuProductDto>> GetFilteredAsync(ProductFiltersDto filtersDto, SortOption sortOption);
+    Task<List<SearchedProductDto>> GetSearchedAsync(string searchValue);
+    Task<Product?> GetByIdAsync(Guid id);
+}
