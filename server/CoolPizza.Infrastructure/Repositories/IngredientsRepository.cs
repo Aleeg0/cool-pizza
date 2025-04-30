3ae4d19d59b128b5b19fd0bf2716dbde4f2a7103
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:39:30 +0300

    feat(server/Infrastructure/Repositories): Create Repository for ingredients entity

---
+++ server/CoolPizza.Infrastructure/Repositories/IngredientsRepository.cs
@@ -0,0 +1,18 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.DTOs.Projections;
+using CoolPizza.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolPizza.Infrastructure.Repositories;
+
+public class IngredientsRepository(ApplicationDbContext context) : IIngredientsRepository
+{
+    public Task<List<MenuIngredientDto>> GetAllAsync()
+    {
+         var query = context.Ingredients
+            .Select(ingredient => new MenuIngredientDto(ingredient.Id, ingredient.Name))
+            .ToListAsync();
+         
+         return query;
+    }
+}
