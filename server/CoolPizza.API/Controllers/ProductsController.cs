88fed4d5d2051b25309cf9ef86a7a5d5524dfd61
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:14:44 +0300

    feat(server/API/Controllers): add ProductsController with 3 Get and 1 Post  request

---
+++ server/CoolPizza.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using CoolPizza.Application.Products.DTOs;
+using CoolPizza.Application.Products.Queries;
+using CoolPizza.Core.DTOs.Projections;
+using CoolPizza.Core.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(IMediator mediator) : ControllerBase
+{
+    [HttpGet("menu")]
+    public async Task<ActionResult<MenuProductsDto>> GetMenuProducts([FromQuery] GetMenuProductsQuery query)
+    {
+        var products = await mediator.Send(query);
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ProductWithVariationsDto>> GetById([FromRoute] GetProductByIdQuery query)
+    {
+        var product = await mediator.Send(query);
+        return Ok(product);
+    }
+    
+    [HttpGet("search")]
+    public async Task<ActionResult<List<SearchedProductDto>>> GetSearchedProducts([FromQuery] GetSearchedProductsQuery query)
+    {
+        var products = await mediator.Send(query);
+        return Ok(products);
+    }
+    
+    [HttpPost("menu/filtered")]
+    public async Task<ActionResult<MenuProductsDto>> GetFilteredMenuProducts([FromBody] GetFilteredMenuProductsQuery query, [FromQuery] SortOption sortBy)
+    {
+        query.SortBy = sortBy;
+        var products = await mediator.Send(query);
+        return Ok(products);
+    }
+}
