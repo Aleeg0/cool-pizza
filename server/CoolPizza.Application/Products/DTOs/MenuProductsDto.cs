b89add44469767cba6b9846cf1e14ea805e6b83a
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:09:05 +0300

    feat(server/Application/Products/DTOs): add DTOs for responses Add MenuProductsDto which contains Category with products of this category and ProductWithVariationsDto which request only 1 object

---
+++ server/CoolPizza.Application/Products/DTOs/MenuProductsDto.cs
@@ -0,0 +1,11 @@
+﻿using CoolPizza.Core.DTOs.Projections;
+using CoolPizza.Core.Entities;
+
+namespace CoolPizza.Application.Products.DTOs;
+
+public record MenuProductsDto
+(
+    Category Category,
+    List<MenuProductDto> MenuProducts
+);
+
---
+++ server/CoolPizza.Application/Products/DTOs/ProductWithVariationsDto.cs
@@ -0,0 +1,17 @@
+﻿using CoolPizza.Core.Entities.Products;
+using CoolPizza.Core.Enums;
+
+namespace CoolPizza.Application.Products.DTOs;
+
+public record ProductWithVariationsDto
+(
+    Guid Id,
+    string Name,
+    string Description,
+    string BaseImg,
+    decimal BasePrice,
+    ProductType ProductType,
+    DateTime CreatedAt,
+    Guid CategoryId,
+    List<Variation> Variations
+);
