413462510eb371f9e89a7bd8e0706bad17bd4270
Aleego <Sankafoxe@gmail.com>
2025-04-30 10:39:21 +0300

    feat(server/Infrastructure/Repositories): Create Repository for category entity

---
+++ server/CoolPizza.Infrastructure/Repositories/CategoriesRepository.cs
@@ -0,0 +1,22 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.Entities;
+using CoolPizza.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolPizza.Infrastructure.Repositories;
+
+public class CategoriesRepository(ApplicationDbContext context) : ICategoriesRepository
+{
+    public async Task<List<Category>> GetAllAsync()
+    {
+        return await context.Categories.ToListAsync();
+    }
+
+    public async Task<Category> CreateAsync(string name, string value)
+    {
+        Category category = Category.Create(name, value);
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
+        return category;
+    }
+}
