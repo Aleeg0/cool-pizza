7211a60b576a1e55e5353daa6c2e76af3a610028
Aleego <Sankafoxe@gmail.com>
2025-05-01 16:40:28 +0300

    feat(server/Core/Abstraction): add GetByIdAsync, UpdateById,DeleteById methods for ICategoriesRepository

--- server/CoolPizza.Core/Abstractions/ICategoriesRepository.cs
+++ server/CoolPizza.Core/Abstractions/ICategoriesRepository.cs
@@ -5,6 +5,8 @@ namespace CoolPizza.Core.Abstractions;
 public interface ICategoriesRepository
 {
     Task<List<Category>> GetAllAsync();
-
+    Task<Category?> GetByIdAsync(Guid id);
     Task<Category> CreateAsync(string name, string value);
+    Task<Category?> UpdateById(Guid id, string? name, string? value);
+    Task<bool> DeleteById(Guid id);
 }
