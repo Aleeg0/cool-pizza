ff98824d233bbcb43a886249ffe788b05ee28cc1
Aleego <Sankafoxe@gmail.com>
2025-05-02 23:53:57 +0300

    ref(client/component/features/Sorter): move sort logic from main page to Sorter component

--- client/src/components/features/Sorter/Sorter.tsx
+++ client/src/components/features/Sorter/Sorter.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, {FC, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Portal} from "@/components/layout";
 import {cn} from "@/utils";
 import {SortIcon} from "@/components/icons";
@@ -7,20 +7,38 @@ import {InlineButton} from "@/components/ui";
 import SorterPopup from "./SorterPopup";
 import styles from './Sorter.module.scss'
 import {sortOptions} from "./lib";
+import {SortByValues} from "@/store/consts/SortByValues";
+import {setSortBy} from "@/store/model/Products";
+import {useRouter, useSearchParams} from "next/navigation";
+import {useAppDispatch, useAppSelector} from "@/store/lib/hooks";
+import {selectSortBy} from "@/store/model/Products";
 
-interface Props {
-  onSortChange: (sortBy: string) => void;
-  sortBy: string;
-}
-
-const Sorter: FC<Props> = ({onSortChange, sortBy}) => {
+const Sorter = () => {
+  // state
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const anchorRef = useRef<HTMLDivElement>(null);
+  // redux
+  const dispatch = useAppDispatch();
+  const sortBy = useAppSelector(selectSortBy);
+  // rout
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const onSelect = (value: string) => {
     setIsOpen(false);
-    onSortChange(value);
+    handleSortChange(value);
   }
+
+  const handleSortChange = (value: string) => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    if (value === SortByValues.NEWEST) {
+      newParams.delete('sortBy');
+    } else {
+      newParams.set('sortBy', value);
+    }
+    router.push(`?${newParams.toString()}`);
+    dispatch(setSortBy(value));
+  };
 
   const sortByName = sortOptions.find(sortOption => sortOption.value === sortBy)?.name ?? "";
 
