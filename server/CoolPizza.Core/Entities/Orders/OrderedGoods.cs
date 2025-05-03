04a15226317b0ed71e43a329f97c092a35e14678
Aleego <Sankafoxe@gmail.com>
2025-05-04 01:36:06 +0300

    feat(server/Core/Entities/Orders): add navigation property and Create and Update business logic to OrderGoods and OrderPizza

--- server/CoolPizza.Core/Entities/Orders/OrderedGoods.cs
+++ server/CoolPizza.Core/Entities/Orders/OrderedGoods.cs
@@ -1,8 +1,27 @@
-﻿namespace CoolPizza.Core.Entities.Orders;
+﻿using CoolPizza.Core.Entities.Products;
+
+namespace CoolPizza.Core.Entities.Orders;
 
 public class OrderedGoods: Entity
 {
     public int Quantity {get; private set;}
     public Guid GoodsId {get; private set;}
+    public Goods Goods {get; private set;} = null!;
     public Guid OrderId {get; private set;}
+
+    public static OrderedGoods Create(Guid orderId, Goods goods)
+    {
+        return new OrderedGoods()
+        {
+            Quantity = 1,
+            GoodsId = goods.Id,
+            Goods = goods,
+            OrderId = orderId
+        };
+    }
+    
+    public void UpdateQuantity(int newQuantity)
+    {
+        Quantity = newQuantity;
+    }
 }
