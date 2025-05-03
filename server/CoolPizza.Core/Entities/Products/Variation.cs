64a42f4a18f104c5477e485641c20cbd9d8adcf9
Aleego <Sankafoxe@gmail.com>
2025-05-03 15:36:20 +0300

    ref(server/Core/Entities): remove common class for different variations. Nwo for serialization all types cast to object, that seems cleaner and more logically

--- server/CoolPizza.Core/Entities/Products/Variation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoolPizza.Core.Entities.Products;
-
-public abstract class Variation: Entity
-{
-    public string ImgUrl { get; private set; }
-    public decimal Price { get; private set; }
-    public Guid ProductId { get; private set; }
-}
