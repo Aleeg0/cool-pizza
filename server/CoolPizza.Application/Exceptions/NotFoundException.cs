6af78d5390f18569eac7a7a61ca8f63a5edd46cc
Aleego <Sankafoxe@gmail.com>
2025-05-04 01:41:16 +0300

    feat(server/Application/Exceptions): add new constructor for NotFoundException

--- server/CoolPizza.Application/Exceptions/NotFoundException.cs
+++ server/CoolPizza.Application/Exceptions/NotFoundException.cs
@@ -7,4 +7,6 @@ public class NotFoundException : Exception
 
     public NotFoundException(string name, object key)
         : base($"Entity \"{name}\" ({key}) was not found.") { }
+    
+    public NotFoundException(string message) : base(message) { }
 }
