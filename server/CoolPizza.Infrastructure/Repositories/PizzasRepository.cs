04e64edb8bb0c2f49f18706ec83d80b48537c9da
Aleego <Sankafoxe@gmail.com>
2025-05-05 00:30:33 +0300

    feat(server/Infrastructure/Repositories): add realization of IPizzasRepository

---
+++ server/CoolPizza.Infrastructure/Repositories/PizzasRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.Entities.Products;
+using CoolPizza.Infrastructure.Data;
+
+namespace CoolPizza.Infrastructure.Repositories;
+
+public class PizzasRepository(ApplicationDbContext context) : IPizzasRepository
+{
+    public async Task<Pizza?> FindByIdAsync(Guid id) => 
+        await context.Pizzas.FindAsync(id);
+}
