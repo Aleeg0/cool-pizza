2802722d551d900625a7a37d7323c4be1bb3ac05
Aleego <Sankafoxe@gmail.com>
2025-05-05 00:30:06 +0300

    feat(server/Infrastructure/Repositories): add realization of IOrderedGoodsRepository

---
+++ server/CoolPizza.Infrastructure/Repositories/OrderedGoodsRepository.cs
@@ -0,0 +1,41 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.Entities.Orders;
+using CoolPizza.Core.Entities.Products;
+using CoolPizza.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolPizza.Infrastructure.Repositories;
+
+public class OrderedGoodsRepository(ApplicationDbContext context) : IOrderedGoodsRepository
+{
+    public async Task<OrderedGoods> CreateAsync(Guid cartId, Goods goods)
+    {
+        var orderedGoods = OrderedGoods.Create(cartId, goods);
+        context.OrderedGoods.Add(orderedGoods);
+        await context.SaveChangesAsync();
+        return orderedGoods;
+    }
+
+    public async Task<OrderedGoods?> FindAsync(Guid cartId, Guid goodsId) => 
+        await context.OrderedGoods.FirstOrDefaultAsync(og => og.GoodsId == goodsId && og.OrderId == cartId);
+
+    public async Task<bool> UpdateAsync(Guid id, int quantity)
+    {
+        var rowsAffected = await context.OrderedGoods
+            .Where(og => og.Id == id)
+            .ExecuteUpdateAsync(s => 
+                s.SetProperty(og => og.Quantity, quantity)
+            );
+        
+        return rowsAffected == 1;
+    }
+
+    public async Task<bool> RemoveAsync(Guid id)
+    {
+        var rowsAffected = await context.OrderedGoods
+            .Where(og => og.Id == id)
+            .ExecuteDeleteAsync();
+        
+        return rowsAffected == 1;
+    }
+}
