ba0eb9aa1dcb30f0e2ac73b5d0e9489a85a2a5a1
Aleego <Sankafoxe@gmail.com>
2025-05-05 00:22:20 +0300

    feat(server/Core/Abstractions): add IOrderedPizzasRepository

---
+++ server/CoolPizza.Core/Abstractions/IOrderedPizzasRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoolPizza.Core.Entities;
+using CoolPizza.Core.Entities.Orders;
+using CoolPizza.Core.Entities.Products;
+
+namespace CoolPizza.Core.Abstractions;
+
+public interface IOrderedPizzasRepository
+{
+    Task<OrderedPizza> CreateAsync(Guid cartId, Pizza pizza, List<Ingredient> ingredient);
+    Task<OrderedPizza?> FindAsync(Guid cartId, Guid pizzaId);
+    Task<bool> UpdateAsync(Guid id, int quantity);
+    Task<bool> RemoveAsync(Guid id);
+}
