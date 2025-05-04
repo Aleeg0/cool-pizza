b368c8f2b1a9f87a092d5bdf246a6cc52baebe4d
Aleego <Sankafoxe@gmail.com>
2025-05-05 00:30:25 +0300

    feat(server/Infrastructure/Repositories): add realization of IOrdersRepository

---
+++ server/CoolPizza.Infrastructure/Repositories/OrdersRepository.cs
@@ -0,0 +1,62 @@
+﻿using CoolPizza.Core.Abstractions;
+using CoolPizza.Core.Entities.Orders;
+using CoolPizza.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolPizza.Infrastructure.Repositories;
+
+public class OrdersRepository(ApplicationDbContext context) : IOrdersRepository
+{
+    public async Task<Order> CreateAsync()
+    {
+        var newOrder = new Order();
+        await context.Orders.AddAsync(newOrder);
+        await context.SaveChangesAsync();
+        return newOrder;
+    }
+    
+    public async Task<Order?> GetFullCartByIdAsync(Guid id) =>
+        await context.Orders
+            .Where(o => o.Id == id)
+            .Include(o => o.PizzasLine).ThenInclude(op => op.Pizza)
+            .Include(o => o.PizzasLine).ThenInclude(op => op.Ingredients)
+            .Include(o => o.GoodsLine).ThenInclude(og => og.Goods)
+            .FirstOrDefaultAsync();
+
+    public async Task<Order?> FindByIdAsync(Guid id) => 
+        await context.Orders.FindAsync(id);
+
+    public async Task<decimal> UpdateTotalAmount(Guid id)
+    {
+        // считаем сумму каждой Line
+        decimal calculatedTotalAmount = await UpdatePizzasTotalAmount(id) +
+                                        await UpdateGoodsTotalAmount(id);
+        
+        // обновляем значение
+        await context.Orders
+            .Where(o => o.Id == id)
+            .ExecuteUpdateAsync(s => s
+                .SetProperty(o => o.TotalAmount, calculatedTotalAmount));
+        
+        return calculatedTotalAmount;
+    }
+
+    private async Task<decimal> UpdatePizzasTotalAmount(Guid cartId) =>
+        await context.OrderedPizzas
+            .Where(op => op.OrderId == cartId)
+            .Select(op => new 
+            {
+                PizzaTotal = op.Quantity * op.Pizza.Price,
+                IngredientsTotal = op.Ingredients.Sum(i => i.Price)
+            })
+            .SumAsync(op => op.PizzaTotal + op.IngredientsTotal);
+
+    private async Task<decimal> UpdateGoodsTotalAmount(Guid cartId) => 
+        await context.OrderedGoods
+            .Where(op => op.OrderId == cartId)
+            .Join(context.Goods,
+                og => og.GoodsId,
+                g => g.Id,
+                (og, g) => og.Quantity * g.Price)
+            .SumAsync();
+}
