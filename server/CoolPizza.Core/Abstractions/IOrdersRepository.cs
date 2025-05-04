28d46f375fddb332463fd011953f8e6aab2bd2bc
Aleego <Sankafoxe@gmail.com>
2025-05-05 00:22:27 +0300

    feat(server/Core/Abstractions): add IOrdersRepository

---
+++ server/CoolPizza.Core/Abstractions/IOrdersRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoolPizza.Core.Entities.Orders;
+
+namespace CoolPizza.Core.Abstractions;
+
+public interface IOrdersRepository
+{
+    Task<Order> CreateAsync();
+    Task<Order?> GetFullCartByIdAsync(Guid id);
+    Task<Order?> FindByIdAsync(Guid id);
+    Task<decimal> UpdateTotalAmount(Guid id);
+}
