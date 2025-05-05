6a3dcb7b7122a855dc2cdd95277626182af0321d
Aleego <Sankafoxe@gmail.com>
2025-05-05 23:18:35 +0300

    feat(store/api): add interceptors for cart cookies

--- client/src/store/apis/api.ts
+++ client/src/store/apis/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5116/api/',
@@ -6,5 +7,29 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.request.use((config) => {
+  if (config.url?.includes('cart')) { // Проверяем URL
+    const token = Cookies.get('X-Cart-Token');
+    if (token) {
+      config.headers['X-Cart-Token'] = token;
+    }
+  }
+  return config;
+});
+
+api.interceptors.response.use((response) => {
+  if (response.config.url?.includes('cart')) {
+    const newToken = response.headers['x-cart-token'];
+    if (newToken) {
+      Cookies.set('X-Cart-Token', newToken, {
+        expires: 3,
+        path: '/',
+        sameSite: 'strict'
+      });
+    }
+  }
+  return response;
+})
 
 export default api;
