21043336516047e9d9efe18ba3c8df3db1038ec5
Aleego <Sankafoxe@gmail.com>
2025-05-05 23:48:21 +0300

    ref(client/store/models): update import of DefaultState to correct

--- client/src/store/model/Ingredients/types.ts
+++ client/src/store/model/Ingredients/types.ts
@@ -1,4 +1,4 @@
 import {Ingredient} from "@/store/types/Ingredient";
-import {DefaultState} from "@/store/types/shared";
+import {DefaultState} from "@/store/model/Shared";
 
 export type IngredientsState = DefaultState<Ingredient[]>;
