3693bfc30827a7776b553f0ed3eeec1abcef4c5b
Aleego <Sankafoxe@gmail.com>
2025-05-05 23:22:19 +0300

    feat(store/model/Shared): add index to reexport matchers and handlers

---
+++ client/src/store/model/Shared/index.ts
@@ -0,0 +1,14 @@
+export {
+  isPendingAction,
+  isRejectedAction
+} from './matchers';
+
+export {
+  handlePending,
+  handleFulfilled,
+  handleRejected,
+} from './handlers';
+
+export type {
+  DefaultState
+} from './types';
