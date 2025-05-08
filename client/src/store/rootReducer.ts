24b3d4ba2c7b6a3bdd437beae4eb2f4a71767d46
Aleego <Sankafoxe@gmail.com>
2025-05-08 11:14:43 +0300

    feat(client/store): add orderForm reducer to RootReducers

--- client/src/store/rootReducer.ts
+++ client/src/store/rootReducer.ts
@@ -3,12 +3,14 @@ import {ingredientsReducer} from "@/store/model/Ingredients";
 import {ProductReducer} from "@/store/model/Products";
 import {CurrentProductReducer} from "@/store/model/CurrentProduct";
 import {CartReducer} from "@/store/model/Cart";
+import {OrderFormReducer} from "@/store/model/OrderForm";
 
 const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
   products: ProductReducer,
   currentProduct: CurrentProductReducer,
-  cart: CartReducer
+  cart: CartReducer,
+  orderForm: OrderFormReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
