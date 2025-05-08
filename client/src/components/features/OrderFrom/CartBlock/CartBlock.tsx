f44bd67d549dbd2d263cac25a230ae0e5ea082fb
Aleeg0 <Sankafoxe@gmail.com>
2025-05-08 21:46:45 +0300

    feat(client/components/features/OrderForm/CartBlock): remove static data, add logic with props for dynamic

--- client/src/components/features/OrderFrom/CartBlock/CartBlock.tsx
+++ client/src/components/features/OrderFrom/CartBlock/CartBlock.tsx
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, {FC} from 'react';
 import BlockContainer from "@/components/features/OrderFrom/BlockContainer/BlockContainer";
 import {OrderCard} from '@/components/entities/Order'
 import styles from "./styles.module.scss";
+import {CartItem} from "@/store/model/Cart";
 
-const CartBlock = () => {
+interface Props {
+  cartItems: CartItem[];
+}
+
+const CartBlock: FC<Props> = ({cartItems}) => {
   return (
     <BlockContainer
       title="1. Корзина"
     >
-      <div className={styles.container}>
+      <div className={styles.content}>
         <ul className={styles.orderItems}>
-          <li className={styles.orderItem}>
-            <OrderCard
-              name="Чизбургер-пицца"
-              details="Средняя 30 см, традиционное тесто"
-              price={965}
-              currency="руб."
-              quantity={2}
-              imgUrl="https://media.dodostatic.net/image/r:366x366/11eef45fda55224990fb822cd755bf1a.avif"
-            />
-          </li>
-          <li className={styles.orderItem}>
-            <OrderCard
-              name="Чизбургер-пицца"
-              details="Средняя 30 см, традиционное тесто"
-              price={965}
-              currency="руб."
-              quantity={2}
-              imgUrl="https://media.dodostatic.net/image/r:366x366/11eef45fda55224990fb822cd755bf1a.avif"
-            />
-          </li>
+          {cartItems.map((item) =>
+            <li
+              className={styles.orderItem}
+              key={item.id}
+            >
+              <OrderCard
+                currency={'руб.'}
+                {...item}
+              />
+            </li>
+          )}
         </ul>
       </div>
     </BlockContainer>
