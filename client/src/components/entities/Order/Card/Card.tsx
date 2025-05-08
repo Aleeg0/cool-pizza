b605cdf8e0b3ae19e67206e6d33f89a1b13c3689
Aleego <Sankafoxe@gmail.com>
2025-05-08 11:28:09 +0300

    feat(client/components/entities/Order/Card): add OrderCard component with styles for OrderFrom and Orders

---
+++ client/src/components/entities/Order/Card/Card.tsx
@@ -0,0 +1,57 @@
+import React, {FC} from 'react';
+import styles from './styles.module.scss'
+
+interface Props {
+  name: string;
+  details : string;
+  addedIngredientsLine : string;
+  imgUrl : string;
+  price : number;
+  quantity : number;
+  currency : string;
+}
+
+const Card: FC<Props> = ({
+  name,
+  details,
+  addedIngredientsLine,
+  imgUrl,
+  price,
+  quantity,
+  currency,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.productInfo}>
+          <div className={styles.productInfo_imageCover}>
+            <img
+              className={styles.productInfo_imageCover__image}
+              src={imgUrl}
+              alt=""
+              loading="lazy"
+            />
+          </div>
+          <div className={styles.productInfo_description}>
+            <div className={styles.productInfo_description__title}>
+              {name}
+            </div>
+            <div className={styles.productInfo_description__details}>
+              {details}
+            </div>
+          </div>
+        </div>
+        <div className={styles.orderInfo}>
+          <div className={styles.orderInfo_price}>
+            {`${price} ${currency}`}
+          </div>
+          <div className={styles.orderInfo_quantity}>
+            {`${quantity} шт.`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
