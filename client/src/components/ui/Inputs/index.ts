4b41b8aa0f3ecab2db994fab727fde1f340a91c1
Aleego <Sankafoxe@gmail.com>
2025-05-09 22:16:11 +0300

    feat(client/components/ui/inputs): rewrite imports

---
+++ client/src/components/ui/Inputs/index.ts
@@ -0,0 +1,5 @@
+export {default as TitledInput} from './TitledInput';
+export {default as TitledErrorInput} from './TitledErrorInput';
+export {default as TitledDropdownInput} from './TitledDropdownInput';
+export {default as UiInput} from './UiInput';
+export {default as IconInput} from './IconInput';
--- client/src/components/ui/Inputs/index.tsx
+++
@@ -1,3 +0,0 @@
-export {NumericInput} from './ui/NumericInput';
-export {Input} from './ui/Input';
-export {TitledInput} from './ui/TitledInput';
