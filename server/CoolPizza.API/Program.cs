1c0ee8e7707211231ebcbd0772d376ae2e900e14
Aleego <Sankafoxe@gmail.com>
2025-05-09 23:20:46 +0300

    feat(server/Api): add httpClient service to DI

--- server/CoolPizza.API/Program.cs
+++ server/CoolPizza.API/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddHttpClient();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(
