2cf2dac8a62e6cf0e8d89f6de382cbd3c89d72ef
Aleego <Sankafoxe@gmail.com>
2025-05-11 13:28:32 +0300

    feat(server/Api): add JwtAuthMiddleware to take userId from accessToken

---
+++ server/CoolPizza.API/MiddleWares/JwtAuthMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace PizzaService.API.MiddleWares;
+
+public class JwtAuthMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.User.Identity?.IsAuthenticated == true)
+        {
+            var userIdClaim = context.User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim != null && Guid.TryParse(userIdClaim.Value, out var userId))
+            {
+                context.Items["UserId"] = userId;
+            }
+        }
+
+        await next(context);
+    }
+    
+}
