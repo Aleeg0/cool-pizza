790242b17ec3e0f0cf0f6e5e170207424fb2cb2c
Aleego <Sankafoxe@gmail.com>
2025-05-11 10:33:21 +0300

    feat(client/store/model/User): add index for User model

---
+++ client/src/store/model/User/index.ts
@@ -0,0 +1,21 @@
+export {
+  selectUser
+} from './selector';
+
+export {
+  default as UserReducer,
+} from './slice';
+
+export {
+  checkAuth,
+  register,
+  login,
+  logout
+} from './thunk';
+
+export type {
+  User,
+  RegisterRequest,
+  LoginRequest,
+  AuthResponse
+} from './types';
