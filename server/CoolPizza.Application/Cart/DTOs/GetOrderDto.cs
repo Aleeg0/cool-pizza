e0cdf157886cee4abef1b791ce5f6d363d2b3852
Aleego <Sankafoxe@gmail.com>
2025-05-12 10:21:43 +0300

    feat(server/Application/Cart): add new query method GetOrdersByUserId with 2 DTOs for response

---
+++ server/CoolPizza.Application/Cart/DTOs/GetOrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolPizza.Application.Cart.DTOs;
+
+public record GetOrderDto
+(
+    Guid Id,
+    decimal TotalAmount,
+    DateTime OrderedAt,
+    List<CartItemDto> PizzaCartLines,
+    List<CartItemDto> GoodsCartLines
+);
