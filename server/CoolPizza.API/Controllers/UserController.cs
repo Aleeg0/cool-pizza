7a937f577bc9a77ed84c52f9f85d4948d884f4db
Aleeg0 <Sankafoxe@gmail.com>
2025-05-13 11:37:46 +0300

    feat(server/Api/Controllers): Add user controller with get and put methods

---
+++ server/CoolPizza.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using CoolPizza.Application.User.Commands;
+using CoolPizza.Application.User.DTOs;
+using CoolPizza.Application.User.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetUser()
+    {
+        var query = new GetUserQuery()
+        {
+            UserId = (Guid?)HttpContext.Items["UserId"]
+        };
+        var user = await mediator.Send(query);
+        return Ok(user);
+    }
+    
+    [HttpPut]
+    public async Task<ActionResult<UserDto>> Put([FromBody] UpdateUserCommand command)
+    {
+        command.Id = (Guid?)HttpContext.Items["UserId"];
+        var result = await mediator.Send(command);
+        return Ok(result);
+    }
+}
