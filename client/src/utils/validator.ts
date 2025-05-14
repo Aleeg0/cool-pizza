4cb1ac18cf897ae477e0eb28d3dc9b755afb1716
Aleego <Sankafoxe@gmail.com>
2025-05-14 19:06:53 +0300

    feat(client/utils): add new method to validator class

--- client/src/utils/validator.ts
+++ client/src/utils/validator.ts
@@ -1,4 +1,5 @@
 class Validator {
+  public readonly PASSWORD_MIN_LENGTH = 8;
 
   public isValidPhone(value: string): boolean {
     return /^[\d\+][\d\s\-\(\)]{7,}$/.test(value);
@@ -10,6 +11,10 @@ class Validator {
 
   public isValidEmail(value?: string): boolean {
     return !!value && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  }
+
+  public isValidPassword(password?: string) {
+    return !!(password && password.trim().length >= this.PASSWORD_MIN_LENGTH);
   }
 }
 
